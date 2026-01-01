f91cf2892a950f82953364c8f9a8671cded4349c
Kai Chen <remlostime@gmail.com>
2025-12-31 16:20:24 -0800

    new files

---
+++ Resignal-iOS/Resignal/ResignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResignalApp.swift
+//  Resignal
+//
+//  Created by Kai Chen on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ResignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
