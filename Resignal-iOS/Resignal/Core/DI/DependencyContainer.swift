b0a26c328073120701e8bb8634c2f95760c4440d
Kai Chen <remlostime@gmail.com>
2026-02-01 21:49:41 -0800

    App Icon

--- Resignal-iOS/Resignal/Core/DI/DependencyContainer.swift
+++ Resignal-iOS/Resignal/Core/DI/DependencyContainer.swift
@@ -122,8 +122,7 @@ final class DependencyContainer: DependencyContainerProtocol {
     // MARK: - Private Methods
     
     private func createAIClient() -> any AIClient {
-//        ResignalAIClient()
-        MockAIClient()
+        ResignalAIClient()
     }
     
     /// Creates a container for previews and testing with in-memory storage
